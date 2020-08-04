25aeb2553ad4c3115e0e81903b82b6cdd6a0168d
ly.yu <ly.yu@lsalab.cs.nthu.edu.tw>
2020-08-04 09:52:45 +0800

    get pybind11 to build with virtual environment and use CUDA

--- python/_elf.cc
+++ python/_elf.cc
@@ -1,6 +1,7 @@
 #include <pybind11/pybind11.h>
 
 #include "controller.h"
+#include "worker_impl.h"
 
 namespace py = pybind11;
 
@@ -9,4 +10,6 @@ PYBIND11_MODULE(_elf, m) {
     m.def("export_controller", &elf::export_controller);
     py::class_<elf::Controller>(m, "Controller");
     py::class_<elf::ExportedController>(m, "ExportedController");
+
+    py::class_<elf::Worker>(m, "Worker");
 }
