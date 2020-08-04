044a1d9b2e8fb70fc2bfc978365a6149ddabd80d
ly.yu <ly.yu@lsalab.cs.nthu.edu.tw>
2020-08-04 09:52:45 +0800

    worker binding

--- python/_elf.cc
+++ python/_elf.cc
@@ -2,6 +2,7 @@
 
 #include "controller.h"
 #include "worker_impl.h"
+#include "operator.h"
 
 namespace py = pybind11;
 
@@ -11,5 +12,13 @@ PYBIND11_MODULE(_elf, m) {
     py::class_<elf::Controller>(m, "Controller");
     py::class_<elf::ExportedController>(m, "ExportedController");
 
-    py::class_<elf::Worker>(m, "Worker");
+    py::class_<elf::Worker>(m, "Worker")
+        .def(py::init<elf::Controller *>())
+        .def("commit_and_join", &elf::Worker::commit_and_join, py::arg("name") = "")
+        .def("leave", &elf::Worker::leave)
+        .def("add_global_variable", &elf::Worker::add_global_variable)
+        .def("add_weight_variable", &elf::Worker::add_weight_variable)
+        .def("begin_batch", &elf::Worker::begin_batch);
+
+    py::class_<elf::Operator>(m, "Operator");
 }
--- test_python.py
+++ test_python.py
@@ -3,9 +3,27 @@ import unittest
 import _elf
 
 
-class CoreTest(unittest.TestCase):
+class BindingTest(unittest.TestCase):
+    """just testing the binding works"""
+
     def test_create_controller(self):
         ctrl = _elf.create_controller()
+
+    def test_export_controller(self):
+        ctrl = _elf.create_controller()
+        ectrl = _elf.export_controller(ctrl, '127.0.0.1:')
+
+    def test_worker(self):
+        ctrl = _elf.create_controller()
+        worker = _elf.Worker(ctrl)
+
+        allreduce = worker.add_global_variable('var1')
+        allreduce = worker.add_weight_variable('var1')
+
+        worker.commit_and_join()
+
+        self.assertEqual(worker.begin_batch()[:2], (True, False))
+
 
 
 if __name__ == '__main__':
